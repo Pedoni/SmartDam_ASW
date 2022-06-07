a0d390f27bf70b52478ad88f7637eba81e700916
Ledmington <ledmington.dev@gmail.com>
2022-06-07 17:09:52 +0200

    Converted dam_controller into a little python container

---
+++ dam_controller/sensor.py
@@ -0,0 +1,23 @@
+import time
+import random
+import math
+import requests
+
+
+def main():
+    n = random.uniform(0, 5)
+    while True:
+        time.sleep(5)
+        x = math.sin(n/3) * 50 + 150 # generates values inside [100;200]
+        n += 1
+        print("[sensor]: sending " + str(x))
+        _ = requests.post(
+            "http://dam_backend:3000/api/data",
+            json={"value": "{:.2f}".format(x)},
+        )
+
+
+if __name__ == "__main__":
+    time.sleep(10)
+    print("[sensor]: started sensor")
+    main()
