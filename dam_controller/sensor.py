e121fdfac9e4760515cec72718608a36bbb2845b
Ledmington <ledmington.dev@gmail.com>
2022-06-10 11:03:16 +0200

    Added replication and quiet mode to sensors

--- dam_controller/sensor.py
+++ dam_controller/sensor.py
@@ -1,16 +1,18 @@
 import time
 import random
 import math
+import sys
 import requests
 
 
-def main():
+def main(quiet=False):
     n = random.uniform(0, 5)
     while True:
         time.sleep(5)
         x = math.sin(n / 3) * 50 + 100  # generates values inside [50;150]
         n += 1
-        print("[sensor]: sending " + str(x))
+        if not quiet:
+            print("[sensor]: sending " + str(x))
         _ = requests.post(
             "http://dam_backend:3000/api/data",
             json={"value": "{:.2f}".format(x)},
@@ -19,5 +21,10 @@ def main():
 
 if __name__ == "__main__":
     time.sleep(random.uniform(5, 15))
-    print("[sensor]: started sensor")
-    main()
+    if len(sys.argv) >= 2 and sys.argv[1] == "quiet":
+        quiet = True
+    else:
+        quiet = False
+    if not quiet:
+        print("[sensor]: started sensor")
+    main(quiet)
