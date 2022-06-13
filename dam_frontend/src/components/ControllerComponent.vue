9ca08472f5a964c158e4b7caa0bacb0b7f574d62
Ledmington <ledmington.dev@gmail.com>
2022-06-13 21:55:33 +0200

    Minor fixes in ControllerComponent

--- dam_frontend/src/components/ControllerComponent.vue
+++ dam_frontend/src/components/ControllerComponent.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <h1 id="controller"></h1>
-    <div id="speedometer"></div>
-    <div class="buttons">
-      <button @click="setPercentage(0)">close</button>
-      <button @click="setPercentage(20)">20 %</button>
-      <button @click="setPercentage(40)">40 %</button>
-      <button @click="setPercentage(60)">60 %</button>
-      <button @click="setPercentage(80)">80 %</button>
-      <button @click="setPercentage(100)">full open</button>
+    <div>
+        <h1 id="controller"></h1>
+        <div id="speedometer"></div>
+        <div class="buttons">
+            <button @click="setPercentage(0)">close</button>
+            <button @click="setPercentage(20)">20 %</button>
+            <button @click="setPercentage(40)">40 %</button>
+            <button @click="setPercentage(60)">60 %</button>
+            <button @click="setPercentage(80)">80 %</button>
+            <button @click="setPercentage(100)">full open</button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -18,79 +18,74 @@ import ApexCharts from "apexcharts";
 const axios = require("axios").default;
 
 export default {
-  data() {
-    return {
-      options: {
-        series: [67],
-        chart: {
-          height: 500,
-          type: "radialBar",
-          offsetY: -10,
-        },
-        plotOptions: {
-          radialBar: {
-            startAngle: -135,
-            endAngle: 135,
-            dataLabels: {
-              name: {
-                fontSize: "16px",
-                color: undefined,
-                offsetY: 120,
-              },
-              value: {
-                offsetY: 76,
-                fontSize: "22px",
-                color: undefined,
-                formatter: function (val) {
-                  return val + "%";
+    data() {
+        return {
+            options: {
+                series: [60],
+                chart: {
+                    height: 500,
+                    type: "radialBar",
+                    offsetY: -10,
                 },
-              },
+                plotOptions: {
+                    radialBar: {
+                        startAngle: -135,
+                        endAngle: 135,
+                        dataLabels: {
+                            name: {
+                                fontSize: "16px",
+                                color: undefined,
+                                offsetY: 120,
+                            },
+                            value: {
+                                offsetY: 76,
+                                fontSize: "22px",
+                                color: undefined,
+                                formatter: function (val) {
+                                    return val + "%";
+                                },
+                            },
+                        },
+                    },
+                },
+                fill: {
+                    type: "gradient",
+                    gradient: {
+                        shade: "dark",
+                        shadeIntensity: 0.15,
+                        inverseColors: false,
+                        opacityFrom: 1,
+                        opacityTo: 1,
+                        stops: [0, 50, 65, 91],
+                    },
+                },
+                stroke: {
+                    dashArray: 4,
+                },
+                labels: ["Dam opening percentage"],
             },
-          },
-        },
-        fill: {
-          type: "gradient",
-          gradient: {
-            shade: "dark",
-            shadeIntensity: 0.15,
-            inverseColors: false,
-            opacityFrom: 1,
-            opacityTo: 1,
-            stops: [0, 50, 65, 91],
-          },
-        },
-        stroke: {
-          dashArray: 4,
-        },
-        labels: ["Dam opening percentage"],
-      },
-    };
-  },
-  mounted() {
-    this.speedometer = new ApexCharts(
-      document.querySelector("#speedometer"),
-      this.options
-    );
-    this.speedometer.render();
-  },
-  methods: {
-    setPercentage(level) {
-      var url = "http://localhost:3000/api/opening";
-      axios
-        .post(url, {
-          percentage: level,
-        })
-        .then((response) => {
-            console.log(response);
-            this.speedometer.updateSeries([level]);
-        });
+        };
     },
-  },
+    mounted() {
+        this.speedometer = new ApexCharts(
+            document.querySelector("#speedometer"),
+            this.options
+        );
+        this.speedometer.render();
+    },
+    methods: {
+        setPercentage(level) {
+            var url = "http://localhost:3000/api/opening";
+            axios
+                .post(url, {
+                    percentage: level,
+                });
+            this.speedometer.updateSeries([level]);
+        },
+    },
 };
 </script>
 
-<style scoped>
-.buttons {
-  text-align: center;
-}
+<style>
+@import '../css/controller.css';
 </style>
