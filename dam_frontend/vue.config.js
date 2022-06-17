de4496fad40726383e59f3edb4ff1099b0c042da
Ledmington <ledmington.dev@gmail.com>
2022-06-17 17:07:58 +0200

    Removed cache because it generated more problems than solutions

--- dam_frontend/vue.config.js
+++ dam_frontend/vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
-        headers: { "Cache-Control": "max-age=31536000" }
-    }
+    transpileDependencies: true
 })
