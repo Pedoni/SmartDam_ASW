02257ac9eb8470282225883076f155e8fae1d2b3
Ledmington <68538713+Ledmington@users.noreply.github.com>
2022-06-18 18:54:03 +0200

    Improved waterlevel testing

--- dam_backend/test/waterlevel.test.js
+++ dam_backend/test/waterlevel.test.js
@@ -31,3 +31,41 @@ test("Normal GET request on /api/waterlevel", async () => {
             expect(response.body.timestamp.length).toBe(10);
         });
 });
+
+test("Expected same level when GETting after POST", async () => {
+    const req = { value: 100 };
+    await request("http://localhost:3000")
+        .post("/api/waterlevel")
+        .send(req)
+        .expect(200);
+
+    await request("http://localhost:3000")
+        .get("/api/waterlevel")
+        .expect(200)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .then(response => {
+            // the last value inserted is in the first position because
+            // the values are returned sorted from the most recent to the oldest
+            expect(response.body.waterlevel[0]).toBe(req.value);
+        });
+});
+
+test("POSTing 10 values and expecting them in same order", async () => {
+    const values = [13, 87, 65, 23, 44, 19, 85, 35, 26, 80];
+    for (let i = 0; i < 10; i++) {
+        await request("http://localhost:3000")
+            .post("/api/waterlevel")
+            .send({
+                value: values[i]
+            })
+            .expect(200);
+    }
+
+    await request("http://localhost:3000")
+        .get("/api/waterlevel")
+        .expect(200)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .then(response => {
+            expect(response.body.waterlevel).toStrictEqual(values.reverse());
+        });
+});
