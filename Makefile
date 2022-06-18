9015edf1440a7f1093ef7f68a5c693062a26b420
Ledmington <68538713+Ledmington@users.noreply.github.com>
2022-06-18 17:54:34 +0200

    Finally the backend has some tests

--- Makefile
+++ Makefile
@@ -4,6 +4,13 @@ dev:
 prod:
 	docker-compose -f compose-prod.yml up --build
 
+test:
+	docker-compose -f compose-test.yml up -d
+	cd dam_backend && npm test
+	cd ..
+	docker-compose -f compose-test.yml down
+
 down:
 	docker-compose -f compose-dev.yml down
 	docker-compose -f compose-prod.yml down
+	docker-compose -f compose-test.yml down
--- dam_backend/test/simple.test.js
+++ dam_backend/test/simple.test.js
@@ -1,61 +1,14 @@
+const request = require("supertest");
 const controller = require("../api/controllers/controller");
-const waterlevel = require("../api/model/waterlevel");
-
-const { MongoClient } = require('mongodb');
-const { ObjectId } = require('mongodb');
 
 jest.setTimeout(20 * 1000);
 
-describe('insert', () => {
-    let connection;
-    let db;
-
-    beforeAll(async () => {
-        connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+test("Expected 400 when POSTing with no value", () => {
+    request("http://localhost:3000")
+        .post("/api/waterlevel")
+        .expect(400)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .then(response => {
+            expect(response.body.message).toBe("value is missing");
         });
-        db = await connection.db(globalThis.__MONGO_DB_NAME__);
-    });
-
-    afterAll(async () => {
-        await connection.close();
-    });
-
-    // it('should insert a doc into collection', async () => {
-    //     const users = db.collection('waterlevels');
-
-    //     const mockUser = { _id: 'some-user-id', name: 'John' };
-    //     await users.insertOne(mockUser);
-
-    //     const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    //     expect(insertedUser).toEqual(mockUser);
-    // });
-
-    it("GET /api/waterlevel", async () => {
-        const post = await waterlevel.create({
-            _id: ObjectId("abcdefghijkl"),
-            timestamp: Date.now(),
-            level: 100
-        });
-        console.log("eheheheh");
-
-        await supertest(app).get("/api/waterlevel/" + post._id)
-            .expect(200)
-            .then((response) => {
-                expect(response.body._id).toBe(post._id);
-                expect(response.body.timestamp).toBe(post.timestamp);
-                expect(response.body.level).toBe(post.level);
-            });
-    });
 });
-
-// test('string with a single number should result in the number itself', () => {
-//     expect(1).toBe(1);
-// });
-
-// // test("Expected 400 if no request", () => {
-// //     expect(
-// //         controller.addNewWaterlevelData(null, new http.ServerReponse(), null)
-// //     ).toBe({});
-// // });
