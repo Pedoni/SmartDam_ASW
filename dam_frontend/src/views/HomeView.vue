fdc48123f3cfdc209e60cfc1c4130946e669b9f6
Ledmington <ledmington.dev@gmail.com>
2022-06-20 00:10:10 +0200

    Added alt tags

--- dam_frontend/src/views/HomeView.vue
+++ dam_frontend/src/views/HomeView.vue
@@ -9,10 +9,10 @@
                 <router-link id="controllerlink" to="/home" replace @click="changeComponent($event, 'Controller')">Controller</router-link>
             </div>
             <div v-if="!isOnDesktop()" id="routerbuttons">
-                <router-link id="homelink" to="/home" replace @click="changeComponent($event, 'Home')"><img class="buttonimage" src="../assets/home.png"/></router-link>
-                <router-link id="panoramiclink" to="/home" replace @click="changeComponent($event, 'Panoramic')"><img class="buttonimage" src="../assets/panorama.png"/></router-link>
-                <router-link id="graphlink" to="/home" replace @click="changeComponent($event, 'Graph')"><img class="buttonimage" src="../assets/graph.png"/></router-link>
-                <router-link id="controllerlink" to="/home" replace @click="changeComponent($event, 'Controller')"><img class="buttonimage" src="../assets/remote.png"/></router-link>
+                <router-link id="homelink" to="/home" replace @click="changeComponent($event, 'Home')"><img class="buttonimage" src="../assets/home.png" alt=""/></router-link>
+                <router-link id="panoramiclink" to="/home" replace @click="changeComponent($event, 'Panoramic')"><img class="buttonimage" src="../assets/panorama.png" alt="" /></router-link>
+                <router-link id="graphlink" to="/home" replace @click="changeComponent($event, 'Graph')"><img class="buttonimage" src="../assets/graph.png" alt="" /></router-link>
+                <router-link id="controllerlink" to="/home" replace @click="changeComponent($event, 'Controller')"><img class="buttonimage" src="../assets/remote.png" alt="" /></router-link>
             </div>
             <div v-if="comp == 'Home'">
                 <HomeComponent></HomeComponent>
@@ -30,7 +30,7 @@
                 <button id="logoutbutton" @click="this.logout">
                     Logout
                 </button>
-                <img id="logoutimage" @click="this.logout" src="../assets/logout.png"/>
+                <img id="logoutimage" @click="this.logout" src="../assets/logout.png" alt="" />
             </footer>
         </div>
         <div class="footer"><p>Smart Dam</p></div>
